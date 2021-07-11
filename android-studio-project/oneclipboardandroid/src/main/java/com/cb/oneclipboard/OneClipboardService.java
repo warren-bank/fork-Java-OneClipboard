94d75c1e1784171298134fede1096bac1ad217bc
Warren R. Bank <warren.r.bank@gmail.com>
2021-07-10 21:39:43 -0700

    wip/android: use NotificationChannel for foreground Service

--- android-studio-project/oneclipboardandroid/src/main/java/com/cb/oneclipboard/OneClipboardService.java
+++ android-studio-project/oneclipboardandroid/src/main/java/com/cb/oneclipboard/OneClipboardService.java
@@ -1,7 +1,11 @@
 package com.cb.oneclipboard;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
@@ -16,13 +20,10 @@ public class OneClipboardService extends Service {
   public int onStartCommand( Intent intent, int flags, int startId ) {
     Log.d( TAG, "onStartCommand called" );
     
-    NotificationCompat.Builder notificationBuilder = ( (ClipboardApplication) getApplicationContext() ).getNotificationBuilder(this);
-    startForeground( ClipboardApplication.NOTIFICATION_ID, notificationBuilder.build() );
-    
+    showNotification();
     isRunning = true;
-    
     start();
-    
+
     return Service.START_STICKY;
   }
 
@@ -33,14 +34,13 @@ public class OneClipboardService extends Service {
   
   @Override
   public void onDestroy() {
-    stopForeground( true );
+    hideNotification();
     isRunning = false;
     super.onDestroy();
   }
 
   @Override
   public IBinder onBind( Intent arg0 ) {
-    // TODO Auto-generated method stub
     return null;
   }
 
@@ -48,4 +48,51 @@ public class OneClipboardService extends Service {
     ( (ClipboardApplication) getApplicationContext() ).initializeClipboardListener();
     ( (ClipboardApplication) getApplicationContext() ).establishConnection();
   }
+
+    // -------------------------------------------------------------------------
+    // foregrounding..
+
+    private Notification getNotification() {
+        NotificationCompat.Builder notificationBuilder = ( (ClipboardApplication) getApplicationContext() ).getNotificationBuilder(this);
+        return notificationBuilder.build();
+    }
+
+    private String getNotificationChannelId() {
+        return getPackageName();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            String channelId       = getNotificationChannelId();
+            NotificationManager NM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel NC = new NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_HIGH);
+
+            NC.setDescription(channelId);
+            NC.setSound(null, null);
+            NM.createNotificationChannel(NC);
+        }
+    }
+
+    private void showNotification() {
+        Notification notification = getNotification();
+
+        if (Build.VERSION.SDK_INT >= 5) {
+            createNotificationChannel();
+            startForeground(ClipboardApplication.NOTIFICATION_ID, notification);
+        }
+        else {
+            NotificationManager NM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NM.notify(ClipboardApplication.NOTIFICATION_ID, notification);
+        }
+    }
+
+    private void hideNotification() {
+        if (Build.VERSION.SDK_INT >= 5) {
+            stopForeground(true);
+        }
+        else {
+            NotificationManager NM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NM.cancel(ClipboardApplication.NOTIFICATION_ID);
+        }
+    }
 }
